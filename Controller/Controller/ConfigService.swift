86c2cd3a2cbe3848e8388d7491317f6ba707102f
ljcooper54 <ljcooper54@yahoo.com>
2025-12-18 14:01:10 -0800

    // Removing Redundant Copy

--- Controller/Controller/ConfigService.swift
+++ Controller/Controller/ConfigService.swift
@@ -1,18 +1,1 @@
-//
-//  ConfigService.swift
-//  MyTrip Planner
-//
-//  Created by Lorne Cooper on 11/6/25.
-//
-
-
-// Copyright 2025 H2so4 Consulting LLC
-import Foundation
-
-/// Provides configuration (API keys) from Info.plist
-enum ConfigService {
-    /// OpenWeather API key from Info.plist (value: $(OPENWEATHER_API_KEY))
-    static func openWeatherAPIKey() -> String {
-        (Bundle.main.object(forInfoDictionaryKey: "OPENWEATHER_API_KEY") as? String) ?? ""
-    } // openWeatherAPIKey
-} // ConfigService
+// Removing Redundant Copy
