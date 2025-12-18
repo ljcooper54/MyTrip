c774a650a40d3b6cfe874438e38cc10e909638e1
ljcooper54 <ljcooper54@yahoo.com>
2025-12-18 14:07:09 -0800

    // Removing Redundant Copy
    Removed redundant comments and copyright information.

--- Controller/Utilities/Logging.swift
+++ Controller/Utilities/Logging.swift
@@ -1,18 +1,1 @@
-//
-//  File.swift
-//  MyTrip Planner
-//
-//  Created by Lorne Cooper on 11/6/25.
-//
-
-// Copyright 2025 H2so4 Consulting LLC
-import Foundation
-
-/// Lightweight debug logger with timestamp and thread tag
-@inline(__always) func dlog(_ tag: String, _ msg: @autoclosure () -> String) {
-    #if DEBUG
-    let t = Thread.isMainThread ? "MAIN" : "BG"
-    let ts = ISO8601DateFormatter().string(from: Date())
-    print("[\(ts)] [\(t)] [\(tag)] \(msg())")
-    #endif
-} // dlog
+// Removing Redundant Copy
